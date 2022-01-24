5f1ea211b686f26ff9c4206543f31966f0ffdc1f
Masato.Kosaka <hmpn0424@gmail.com>
2022-01-24 20:24:05 +0900

    product order and payments

---
+++ orders/urls.py
@@ -0,0 +1,9 @@
+from unicodedata import name
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('place_order/', views.place_order, name='place_order'),
+    path('payments/', views.payments, name="payments"),
+    path('complete/', views.complete, name="complete"),
+]
